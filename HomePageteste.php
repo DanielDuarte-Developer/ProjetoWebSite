bf84191ce9690b0e93c8389b44e6ffe9ba7d0432
MiguelCruz03 <126091723+MiguelCruz03@users.noreply.github.com>
2023-12-11 10:21:43 +0000

    Add files via upload

---
+++ HomePageteste.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/homePageteste.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+    <div id="nav-bar">
+  <input id="nav-toggle" type="checkbox"/>
+  <div id="nav-header"><a id="nav-title" href="https://codepen.io" target="_blank"><i class="fab fa-codepen"></i>Nome Empresa</a>
+    <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
+    <hr/>
+  </div>
+  <div id="nav-content">
+    <div class="nav-button"><i class="fas fa-palette"></i><span>Home</span></div>
+    <div class="nav-button"><i class="fas fa-thumbtack"></i><span>Products</span></div>
+    <hr/>
+    <div class="nav-button"><i class="fas fa-images"></i><span>About Us</span></div>
+    <div class="nav-button"><i class="fas fa-thumbtack"></i><span>Contact</span></div>
+    <hr/>
+    
+    <div id="nav-content-highlight"></div>
+  </div>
+  <input id="nav-footer-toggle" type="checkbox"/>
+  <div id="nav-footer">
+    <div id="nav-footer-heading">
+    <div id="nav-footer-avatar"></div>
+      <div id="nav-footer-titlebox"><a id="nav-footer-title" href="https://codepen.io/uahnbu/pens/public" target="_blank">Conta</a><span id="nav-footer-subtitle">Admin</span></div>
+      <label for="nav-footer-toggle"><i class="fas fa-caret-up"></i></label>
+    </div>
+    <div id="nav-footer-content">
+      <Lorem>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Lorem>
+    </div>
+  </div>
+</div>
+        </div>
+    </header>
+
+    <main>
+      
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
